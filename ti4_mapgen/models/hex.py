727ab659b94561355286583b00e0369dedec85ae
solbero <njord.solberg@gmail.com>
2022-05-28 14:35:27 +0200

    Add method to add two cube positions to models/hex.py

--- ti4_mapgen/models/hex.py
+++ ti4_mapgen/models/hex.py
@@ -14,3 +14,8 @@ class Cube:
     def __post__init__(self) -> None:
         # Perfor sanity check on cube coordinate
         assert sum((self.q, self.r, self.s)) == 0
+
+    @staticmethod
+    def add(a: Cube, b: Cube) -> Cube:
+        """Add two positions in a hexagonal grid."""
+        return Cube(a.q + b.q, a.r + b.r, a.s + b.s)
