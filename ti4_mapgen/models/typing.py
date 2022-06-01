f467417bd6e26e705e7195e54ad9f4594e2e0718
solbero <njord.solberg@gmail.com>
2022-06-01 22:35:39 +0200

    Add modules/typing.py

---
+++ ti4_mapgen/models/typing.py
@@ -0,0 +1,68 @@
+from typing import Literal
+
+Anomaly = Literal[
+    "asteroid-field",
+    "gravity-rift",
+    "nebula",
+    "supernova",
+]
+Tech = Literal[
+    "biotic",
+    "cybernetic",
+    "warfare",
+]
+Trait = Literal[
+    "cultural",
+    "hazardous",
+    "industrial",
+]
+Wormhole = Literal[
+    "alpha",
+    "beta",
+    "gamma",
+]
+
+Back = Literal[
+    "blue",
+    "green",
+    "red",
+]
+
+Letter = Literal[
+    "A",
+    "B",
+]
+
+Release = Literal[
+    "base",
+    "pok",
+    "codex-3",
+]
+
+Name = Literal[
+    "Sardakk N'orr",
+    "The Arborec",
+    "The Argent Flight",
+    "The Barony of Letnev",
+    "The Clan of Saar",
+    "The Council Keleres",
+    "The Embers of Muaat",
+    "The Emirates of Hacan",
+    "The Empyrean",
+    "The Federation of Sol",
+    "The Ghosts of Creuss",
+    "The Lizix Mindnet",
+    "The Mahact Gene-sorcerers",
+    "The Mentak Coalition",
+    "The Naalu Collective",
+    "The Naaz-Rokha Alliance",
+    "The Nekro Virus",
+    "The Nomad",
+    "The Titans of Ul",
+    "The Universities of Jol-Nar",
+    "The Vuil'raith Cabal",
+    "The Winnu",
+    "The Xxcha Kingdom",
+    "The Yin Brotherhood",
+    "The Yssaril Tribes",
+]
