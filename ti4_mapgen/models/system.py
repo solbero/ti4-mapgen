cb0b4826f2c104bf31bc9b9e37cc1f987c5c4286
solbero <njord.solberg@gmail.com>
2022-06-08 19:37:51 +0200

    Fix module import in models/system.py

--- ti4_mapgen/models/system.py
+++ ti4_mapgen/models/system.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from dataclasses import dataclass
 from typing import Optional
 
-from ti4_mapgen.models.typing import Anomaly, Wormhole, Trait, Tech
+from models.typing import Anomaly, Wormhole, Trait, Tech
 
 
 @dataclass(frozen=True)
