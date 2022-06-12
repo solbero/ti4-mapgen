3681c86830a418f70c9b65c70cd61366348aaf5c
solbero <njord.solberg@gmail.com>
2022-06-12 12:32:07 +0200

    Refactor models/tile.py

--- ti4_mapgen/models/tile.py
+++ ti4_mapgen/models/tile.py
@@ -5,29 +5,42 @@ from typing import Optional
 
 from dataclass_wizard import JSONWizard, JSONFileWizard
 
-from .hex import CardinalDirection, CubePosition
-from .typing import Back, Letter, Name, Release, Anomaly, Wormhole, Trait, Tech
+from .hex import CardinalDirection, Cube
+from .typing import Back, Letter, Faction, Release, Anomaly, Wormhole, Trait, Tech, Tag
 
 
-@dataclass(frozen=True, kw_only=True)
+@dataclass(frozen=False, kw_only=True)
 class Tile(JSONWizard, JSONFileWizard):
     """Class representing a tile."""
 
     class _(JSONWizard.Meta):
         skip_defaults = True
 
-    position: Optional[CubePosition] = None
+    tag: Tag
     number: int
     letter: Optional[Letter] = None
-    type: str
-    back: Optional[Back] = None
     release: Release
+    back: Optional[Back] = None
     rotation: Optional[int] = 0
-    faction: Optional[Name] = None
+    system: Optional[System] = None
+
+
+@dataclass()
+class TileFaceDown:
+    """Class representing a face-down tile."""
+    tag: Tag
+    back: Back
+
+
+@dataclass(frozen=True, kw_only=True)
+class System:
+    """Class representing the system in a tile."""
+
+    faction: Optional[Faction] = None
     anomaly: Optional[Anomaly] = None
     wormhole: Optional[Wormhole] = None
-    planets: Optional[list[Planet]] = field(default_factory=list)
-    hyperlanes: list[list[CardinalDirection]] = field(default_factory=list)
+    planets: list[Planet] = field(default_factory=list)
+    hyperlanes: list[list[CardinalDirection | Cube]] = field(default_factory=list)
 
 
 @dataclass(frozen=True, kw_only=True)
