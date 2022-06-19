46482c93d1976f6ef293c0d03a9de32c33b2797f
solbero <njord.solberg@gmail.com>
2022-06-19 14:21:03 +0200

    Refactor models/hex.py

--- ti4_mapgen/models/hex.py
+++ ti4_mapgen/models/hex.py
@@ -1,121 +1,118 @@
 from __future__ import annotations
 
-from typing import Literal, NamedTuple, Optional
-
-from bidict import bidict
-
-
-CardinalDirection = Literal[
-    "SE",
-    "S",
-    "SW",
-    "NW",
-    "N",
-    "NE",
-]
+from collections import deque
+from dataclasses import dataclass
+from enum import Enum
+from collections.abc import Iterator, Sequence
+from typing import overload
 
 
-class Cube(NamedTuple):
+@dataclass(frozen=True, slots=True)
+class Cube:
     "A cube representation of a position or vector in a hexagonal grid."
     q: int
     r: int
     s: int
 
+    def __post_init__(self):
+        # Perform sanity check on coordinate.
+        assert self.q + self.r + self.s == 0, "Sum of 'q', 'r', 's' must be 0"
 
-_CARDINAL_TO_VECTOR: bidict[CardinalDirection, Cube] = bidict(
-    {
-        "N": Cube(0, -1, 1),
-        "NE": Cube(1, -1, 0),
-        "SE": Cube(1, 0, -1),
-        "S": Cube(0, 1, -1),
-        "SW": Cube(-1, 1, 0),
-        "NW": Cube(-1, 0, 1),
-    }
-)
+    def __add__(self, other: Cube) -> Cube:
+        if not isinstance(other, Cube):
+            raise NotImplementedError
+        return Cube(self.q + other.q, self.r + other.r, self.s + other.s)
 
-_INDEX_TO_VECTOR: bidict[int, Cube] = bidict(
-    {
-        0: Cube(0, -1, 1),  # N
-        1: Cube(1, -1, 0),  # NE
-        2: Cube(1, 0, -1),  # SE
-        3: Cube(0, 1, -1),  # S
-        4: Cube(-1, 1, 0),  # SW
-        5: Cube(-1, 0, 1),  # NW
-    }
-)
+    def __sub__(self, other: Cube) -> Cube:
+        if not isinstance(other, Cube):
+            raise NotImplementedError
+        return Cube(self.q - other.q, self.r - other.r, self.s - other.s)
 
+    def __mul__(self, other: int):
+        if not isinstance(other, int):
+            raise NotImplementedError
+        return Cube(self.q * other, self.r * other, self.s * other)
 
-def add(a: Cube, b: Cube) -> Cube:
-    """Add a cube position or vector to a cube position or vector."""
-    return Cube(a.q + b.q, a.r + b.r, a.s + b.s)
+    def __floordiv__(self, other: Cube | int):
+        if not isinstance(other, int):
+            raise NotImplementedError
+        return Cube(int(self.q / other), int(self.r / other), int(self.s / other))
 
-
-def subtract(a: Cube, b: Cube) -> Cube:
-    """Subtract a cube position vector from a cube position or vector."""
-    return Cube(a.q - b.q, a.r - b.r, a.s - b.s)
+    def __abs__(self) -> int:
+        return (abs(self.q) + abs(self.r) + abs(self.s)) // 2
 
 
-def scale(a: Cube, scalar: int) -> Cube:
-    """Scale a cube position or vector by a scalar."""
-    return Cube(a.q * scalar, a.r * scalar, a.s * scalar)
+class Direction(Enum):
+    N = Cube(0, -1, 1)
+    NE = Cube(1, -1, 0)
+    SE = Cube(1, 0, -1)
+    S = Cube(0, 1, -1)
+    SW = Cube(-1, 1, 0)
+    NW = Cube(-1, 0, 1)
 
 
-def vector_from_cardinal(direction: CardinalDirection) -> Cube:
-    """Find the cube vector corresponding to a cardinal direction."""
-    return _CARDINAL_TO_VECTOR[direction]
+class Diagonal(Enum):
+    E = Cube(2, -1, -1)
+    NNE = Cube(1, -2, 1)
+    NNW = Cube(-1, -1, 2)
+    W = Cube(-2, 1, 1)
+    SSW = Cube(-1, 2, -1)
+    SSE = Cube(1, 1, -2)
 
 
-def cardinal_from_vector(vector: Cube) -> CardinalDirection:
-    """Find the cardinal direction corresponding to a cube vector."""
-    return _CARDINAL_TO_VECTOR.inv[vector]
+class Move(Enum):
+    CLOCKWISE = 1
+    COUNTERCLOCKWISE = -1
 
 
-def vector_from_index(index: int) -> Cube:
-    """Find the cube vector corresponding to an index."""
-    return _INDEX_TO_VECTOR[index]
+def adjacent(a: Cube, direction: Direction) -> Cube:
+    """Find an adjacent cube position in direction from initial cube position."""
+    return a + direction.value
 
 
-def index_from_vector(vector: Cube) -> int:
-    """Find the index corresponding to a cube vector."""
-    return _INDEX_TO_VECTOR.inv[vector]
+def diagonal(a: Cube, direction: Diagonal) -> Cube:
+    """Find a diagonal cube position in direction from initial cube position."""
+    return a + direction.value
 
 
-def adjacent(a: Cube, direction: CardinalDirection | Cube) -> Cube:
-    """Find a neighbor cube position in cardinal direction or cube vector direction from initial cube position."""
-    if isinstance(direction, str):
-        vector = vector_from_cardinal(direction)
-    elif isinstance(direction, Cube):
-        vector = direction
-    else:
-        raise ValueError("Argument 'direction' must be 'Cube' or 'CardinalDirection'")
+def ring(
+    center: Cube, radius: int, *, direction: Direction = Direction.N, move: Move = Move.CLOCKWISE
+) -> Iterator[Cube]:
+    """Calculate all positions on a ring which is radius distance from a center position.
 
-    return add(a, vector)
+    The iterator yields cube positions in the ring starting from the passed direction and moving
+    around the ring in passed move direction.
 
+    Args:
+        center: Cube position in center of ring.
+        radius: Ring distance from center position.
+        direction (optional): Direction to start from center position. Defaults to Direction.N.
+        move (optional): Direction to move around the ring. Defaults to Move.CLOCKWISE.
 
-def ring(center: Cube, radius: int, direction: CardinalDirection = "N", clockwise: bool = True) -> list[Cube]:
-    """Calculate all positions on a ring which is radius distance from the center position."""
-    vector = vector_from_cardinal(direction)
-    scaled_vector = scale(vector, radius)
-    position = add(center, scaled_vector)
-    cardinal_directions = list(_CARDINAL_TO_VECTOR.keys())
+    Yields:
+        Cube position in ring.
+    """
+    vector = direction.value
+    scaled_vector = vector * radius
+    position = center + scaled_vector
+    neighbors = deque(direction.value for direction in Direction)
 
-    if not clockwise:
-        cardinal_directions.reverse()
+    if move is Move.COUNTERCLOCKWISE:
+        neighbors.reverse()
 
-    # Rearranges 'cardinal_directions' to the correct order for circeling around a ring, dependant on 'direction'
-    dividend = cardinal_directions.index(direction) + 1
-    divisor = len(cardinal_directions)
-    for _ in range(1, dividend % divisor + 2):
-        first_element = cardinal_directions.pop(0)
-        cardinal_directions.append(first_element)
+    # Rearranges 'neighbors' to the correct order for moving around the ring
+    steps = neighbors.index(vector) + 2
+    neighbors.rotate(-steps)
 
-    results: list[Cube] = []
-    for direction in cardinal_directions:
+    for neighbor in neighbors:
         for _ in range(radius):
-            results.append(position)
-            position = adjacent(position, direction)
+            yield position
+            position = adjacent(position, Direction(neighbor))
 
-    return results
+
+def _rotate_right(a: Cube) -> Cube:
+    """Rotate a cube vector counterclockwise."""
+    return Cube(-a.r, -a.s, -a.q)
 
 
 def _rotate_left(a: Cube) -> Cube:
@@ -123,50 +120,70 @@ def _rotate_left(a: Cube) -> Cube:
     return Cube(-a.s, -a.q, -a.r)
 
 
-def _rotate_right(a: Cube) -> Cube:
-    """Rotate a cube vector clockwise."""
-    return Cube(-a.r, -a.s, -a.q)
+def rotate(a: Cube, center: Cube, *, angle: int = 0, move: Move = Move.CLOCKWISE) -> Cube:
+    """Rotate a cube position or vector around a center position.
 
+    Args:
+        a: Cube position or vector to rotate around a center.
+        center (optional): Cube position to use as the center of rotation.
+        angle (optional): Degrees to rotate a cube position or vector around the center. Defaults to 0.
+        rotate (optional): Direction to perform rotation. Defaults to Move.CLOCKWISE.
 
-def rotate(a: Cube, center: Optional[Cube] = None, angle: int = 0) -> Cube:
-    """Rotate a cube position or vector around a center position."""
-    if not center:
-        center = Cube(0, 0, 0)
+    Raises:
+        ValueError: If 'angle' is not a positive integer.
+        ValueError: If 'angle' is not divisible by 60.
 
-    vector = subtract(a, center)
+    Returns:
+        Rotated cube position.
+    """
+    # Check if 'angle' is a positive number.
+    if angle < 0:
+        raise ValueError("Argument 'angle' must be a positive integer.")
 
-    # Check if 'angle' is divisible by 60, if not raise an error.
+    # Check if 'angle' is divisible by 60.
     if angle % 60 != 0:
         raise ValueError("Argument 'angle' must be in 60 degree increments")
 
-    steps = int((angle % 360) / 60)
+    vector = a - center
+    steps = (angle % 360) // 60
 
-    if steps > 0:
-        *_, vector = tuple(_rotate_right(vector) for _ in range(steps))
-    elif steps < 0:
-        *_, vector = tuple(_rotate_left(vector) for _ in range(steps))
+    for _ in range(steps):
+        if move is Move.CLOCKWISE:
+            vector = _rotate_right(vector)
+        elif move is Move.COUNTERCLOCKWISE:
+            vector = _rotate_left(vector)
 
-    return add(center, vector)
+    return center + vector
 
 
 def length(a: Cube) -> int:
     """Calculate the length of a cube vector."""
-    return abs(a.q) + abs(a.r) + abs(a.s) // 2
+    return abs(a)
 
 
 def distance(a: Cube, b: Cube) -> int:
     """Find the distance between two cube positions."""
-    vector = subtract(a, b)
-    return length(vector)
+    return length(a - b)
 
 
-def spiral(center: Cube, radius: int, direction: CardinalDirection = "N", clockwise: bool = True) -> list[Cube]:
-    """Calculate all positions which is radius distance from the center position."""
-    results: list[Cube] = []
-    results.append(center)
+def spiral(
+    center: Cube, radius: int, *, direction: Direction = Direction.N, move: Move = Move.CLOCKWISE
+) -> Iterator[Cube]:
+    """Calculate all positions in a spiral pattern which is radius distance from a center position.
 
-    steps = range(1, radius + 1)
-    for step in steps:
-        results.extend(ring(center, step, direction, clockwise))
+    Args:
+        center: Cube position in center of spiral.
+        radius: Spiral distance from center position.
+        direction (optional): Direction to start from center position. Defaults to "Direction.N".
+        clockwise (optional): Direction to move around the spiral. Defaults to Rotate.CLOCKWISE.
 
-    return results
+    Yields:
+        Cube position in spiral.
+    """
+
+    yield center
+
+    radiuses = range(1, radius + 1)
+    for radius in radiuses:
+        for position in ring(center, radius, direction=direction, move=move):
+            yield position
