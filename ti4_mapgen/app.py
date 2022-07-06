2e921b51f1b16b5c47cfb54d25a401ae0844ac2b
solbero <njord.solberg@gmail.com>
2022-07-06 15:01:25 +0200

    Finish API endpoints in app.py

--- ti4_mapgen/app.py
+++ ti4_mapgen/app.py
@@ -2,61 +2,109 @@
 
 from __future__ import annotations
 
-from sanic import Request, Sanic, HTTPResponse, text
+from typing import cast
 
-from models.board import Map
-from models.tile import Tile
+import sanic
+from sanic.exceptions import SanicException
 
-app = Sanic(__name__)
+from ti4_mapgen import board, schema
 
-
-@app.before_server_start
-def setup_data(app):
-    app.ctx.maps = Map.from_json_file("./ti4_mapgen/data/map_data.json")
-    app.ctx.tiles = Tile.from_json_file("./ti4_mapgen/data/tile_data.json")
+app = sanic.Sanic(__name__)
 
 
-@app.post("/maps")
-async def maps(request: Request) -> HTTPResponse:
-    maps: list[Map] = app.ctx.maps
-    return text(Map.list_to_json(maps), content_type="application/json")
+@app.get("/maps")
+async def maps(request: sanic.Request) -> sanic.HTTPResponse:
+    maps = cast(list[schema.Map], schema.Map.from_json_file("ti4_mapgen/data/map_data.json"))
+    args = dict(request.args)
+
+    match args:
+        case {"players": players, "style": styles}:
+            results = [map for map in maps if str(map.players.value) in players and map.style in styles]
+        case {"players": players}:
+            results = [map for map in maps if str(map.players.value) in players]
+        case {"style": styles}:
+            results = [map for map in maps if map.style in styles]
+        case _ if args:
+            raise SanicException(
+                message=f"Request contained invalid paramater(s): {', '.join(f'{k!r}' for k in args.keys())}",
+                status_code=404,
+            )
+        case _:
+            results = [map for map in maps]
+
+    exclude = ["layout"]
+    response = [result.to_dict(exclude=exclude) for result in results]
+    return sanic.json(response)
 
 
-@app.post("/maps/<players:int>")
-async def maps_players(request: Request, players: int) -> HTTPResponse:
-    maps: list[Map] = app.ctx.maps
-    filtered_maps = [map for map in maps if map.players == players]
-    return text(Map.list_to_json(filtered_maps), content_type="application/json")
+@app.get("/factions")
+async def factions(request: sanic.Request):
+    factions = cast(list[schema.Faction], schema.Faction.from_json_file("ti4_mapgen/data/faction_data.json"))
+    args = dict(request.args)
+
+    match args:
+        case {"release": releases}:
+            results = [faction for faction in factions if faction.release.value in releases]
+        case _ if args:
+            raise SanicException(
+                message=f"Request contained invalid paramater(s): {', '.join(f'{k!r}' for k in args.keys())}",
+                status_code=404,
+            )
+        case _:
+            results = [faction for faction in factions]
+
+    response = [result.to_dict() for result in results]
+    return sanic.json(response)
 
 
-@app.post("/maps/<players:int>/<style:str>")
-async def layout_players_name(request: Request, players: int, style: str):
-    maps: list[Map] = app.ctx.maps
-    filtered_maps = [map for map in maps if map.players == players and map.style == style]
-    return text(Map.list_to_json(filtered_maps), content_type="application/json")
+@app.get("/tiles")
+async def tiles(request: sanic.Request) -> sanic.HTTPResponse:
+    tiles = cast(list[schema.Tile], schema.Tile.from_json_file("ti4_mapgen/data/tile_data.json"))
+    args = dict(request.args)
+
+    match args:
+        case _ if args:
+            raise SanicException(
+                message=f"Request contained invalid paramater(s): {', '.join(f'{k!r}' for k in args.keys())}",
+                status_code=404,
+            )
+        case _:
+            results = [tile for tile in tiles]
+
+    exclude = ["tag", "position"]
+    response = [result.to_dict(exclude=exclude) for result in results]
+    return sanic.json(response)
 
 
-@app.post("/races")
-async def races(request: Request):
-    pass
+@app.get("/generate")
+async def generate(request: sanic.Request) -> sanic.HTTPResponse:
+    maps = cast(list[schema.Map], schema.Map.from_json_file("ti4_mapgen/data/map_data.json"))
+    tiles = cast(list[schema.Tile], schema.Tile.from_json_file("ti4_mapgen/data/tile_data.json"))
+    args = dict(request.args)
 
+    match args:
+        case {"release": releases, "players": players, "style": styles, "factions": names}:
+            layout = next((map.layout for map in maps if str(map.players.value) in players and map.style in styles))
+            stack = [tile for tile in tiles if tile.front.release.value in releases and tile.tag.value == "system"]
+            home_systems_tiles = [
+                tile
+                for tile in tiles
+                if tile.front.faction and tile.front.faction.value in names and tile.tag.value == "home"
+            ]
+        case {"release": releases, "players": players, "style": styles}:
+            layout = next((map.layout for map in maps if str(map.players.value) in players and map.style in styles))
+            stack = [tile for tile in tiles if tile.front.release.value in releases and tile.tag.value == "system"]
+            home_systems_tiles = []
+        case _:
+            param = ["release", "players", "style"]
+            raise SanicException(
+                message=f"Request missing paramater(s): {', '.join(f'{k!r}' for k in param if k not in args.keys())}",
+                status_code=404,
+            )
 
-@app.post("/races/<release:str>")
-async def races_release(request: Request, release):
-    pass
-
-
-@app.post("/tiles")
-async def tiles(request: Request) -> HTTPResponse:
-    tiles: list[Tile] = app.ctx.tiles
-    return text(Tile.list_to_json(tiles), content_type="application/json")
-
-
-@app.post("/tiles/<number:int>")
-async def tiles_number(request: Request, number: int) -> HTTPResponse:
-    tiles: list[Tile] = app.ctx.tiles
-    filered_tiles = [tile for tile in tiles if tile.number == number]
-    return text(Tile.list_to_json(filered_tiles), content_type="application/json")
+    response = board.Board(layout=layout, stack=stack, home_system_tiles=home_systems_tiles)
+    exclude = ["stack"]
+    return sanic.json(response.to_dict(exclude=exclude))
 
 
 if __name__ == "__main__":
