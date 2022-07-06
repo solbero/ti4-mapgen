166e1cce27a14ed194cac82e2b3959145d4bc590
solbero <njord.solberg@gmail.com>
2022-07-06 15:00:52 +0200

    Rewrite board.py

--- ti4_mapgen/board.py
+++ ti4_mapgen/board.py
@@ -1,62 +1,37 @@
 from __future__ import annotations
 
 import dataclasses
-
 import random
-from copy import deepcopy
 from typing import Optional
 
 import dataclass_wizard
 
-from ti4_mapgen.typing import Faction
+from ti4_mapgen import schema
 
 
-@dataclass()
-class Board(JSONWizard, JSONFileWizard):
+@dataclasses.dataclass()
+class Board(dataclass_wizard.JSONWizard):
     """Class representing a board."""
 
-    map: dataclasses.InitVar[Map]
-    tiles: dataclasses.InitVar[list[Tile]]
-    factions: dataclasses.InitVar[list[Faction]]
+    class _(dataclass_wizard.JSONWizard.Meta):
+        skip_defaults = True
 
-    def __post_init__(self, map: Map, stack: list[Tile], factions: list[Faction]):
-        self.players = map.no_players
-        self.style = map.style
-        self.layout = deepcopy(map.layout)
-        self._setup_board()
+    layout: list[schema.Tile]
+    stack: list[schema.Tile]
+    home_system_tiles: dataclasses.InitVar[list[schema.Tile]]
 
-    def _setup_board(self):
-        """Populate the board with systems."""
-        # Place the Mecatol Rex system (#18) in the center of the board
-        self.immutable_board[self.layout.center] = self.tiles.pop(
-            self.tiles.index(next(tile for tile in self.tiles if tile.number == 18))
-        )
+    def __post_init__(self, home_systems):
+        random.shuffle(self.stack)
+        self._setup(self.layout, self.stack, home_systems)
 
-        for value in self.layout.homes.values():
-            self.immutable_board[value] = None
-
-        """self.layout.center = self.stack.pop(self.stack.index(next(tile for tile in self.stack if tile.number == 18)))
-        board.append(center_position)"""
-
-        # Assign a random system to every system tile.
-        for tile in self.tiles:
-            # Select a random system from the list of unassigned systems.
-            selected_system = random.choice(self.unassigned_tiles)
-            # Remove the selected system from the list of unassigned systems and assign the system to the current tile.
-            self.unassigned_tiles.remove(selected_system)
-            tile.system = selected_system
-
-    def mutate(self, prob_func: Optional[Callable] = None) -> None:
-        # If no probability function is passed to the method, use a default probability function.
-        if prob_func is None:
-            # The probability of mutating a tile is '1/n', where 'n' is the number of possible tiles to mutate.
-            prob_func = lambda collection: random.randint(1, len(collection)) == 1
-
-        # Iterate over all the system tiles.
-        for tile in self.mutable_tiles:
-            # Use the probability function to check if a tile is mutated.
-            if prob_func(self.mutable_tiles):
-                # Select a random system from the list of unassigned systems.
-                selected_system = random.choice(self.unassigned_tiles)
-                # Switch the current system with the selected system.
-                tile.system, selected_system = selected_system, tile.system
+    def _setup(self, layout: list[schema.Tile], stack: list[schema.Tile], home_system_tiles: list[schema.Tile]):
+        """Populate the layout with tiles from the stack."""
+        for index, tile in enumerate(layout):
+            if tile.tag is schema.Tag.HOME and home_system_tiles:
+                home_system = home_system_tiles.pop(0)
+                home_system.position = tile.position
+                layout[index] = home_system
+            elif tile.tag is schema.Tag.SYSTEM and stack:
+                system = stack.pop(0)
+                system.position = tile.position
+                layout[index] = system
