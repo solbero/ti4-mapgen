b2f070b695cd2b54ec6342d2fc84bd0447ba5975
solbero <njord.solberg@gmail.com>
2022-07-11 17:58:33 +0200

    Switch to FastAPI  in app.py

--- ti4_mapgen/app.py
+++ ti4_mapgen/app.py
@@ -1,111 +1,60 @@
-#!/usr/bin/env python
+from typing import Optional
+from deta import Deta
+from fastapi import FastAPI, Form, Query
 
-from __future__ import annotations
+from ti4_mapgen.database import AsyncBase
+from ti4_mapgen.schema import (
+    Color,
+    Faction,
+    Map,
+    MapInDB,
+    TileBase,
+    TileInDB,
+    TileQuery,
+    TileRead,
+    Type,
+    Letter,
+    Release,
+)
 
-from typing import cast
-
-import sanic
-from sanic.exceptions import SanicException
-
-from ti4_mapgen import board, schema
-
-app = sanic.Sanic(__name__)
-
-
-@app.get("/maps")
-async def maps(request: sanic.Request) -> sanic.HTTPResponse:
-    maps = cast(list[schema.Map], schema.Map.from_json_file("ti4_mapgen/data/map_data.json"))
-    args = dict(request.args)
-
-    match args:
-        case {"players": players, "style": styles}:
-            results = [map for map in maps if str(map.players.value) in players and map.style in styles]
-        case {"players": players}:
-            results = [map for map in maps if str(map.players.value) in players]
-        case {"style": styles}:
-            results = [map for map in maps if map.style in styles]
-        case _ if args:
-            raise SanicException(
-                message=f"Request contained invalid paramater(s): {', '.join(f'{k!r}' for k in args.keys())}",
-                status_code=404,
-            )
-        case _:
-            results = [map for map in maps]
-
-    exclude = ["layout"]
-    response = [result.to_dict(exclude=exclude) for result in results]
-    return sanic.json(response)
+app = FastAPI()
+engine = Deta()
 
 
-@app.get("/factions")
-async def factions(request: sanic.Request):
-    factions = cast(list[schema.Faction], schema.Faction.from_json_file("ti4_mapgen/data/faction_data.json"))
-    args = dict(request.args)
-
-    match args:
-        case {"release": releases}:
-            results = [faction for faction in factions if faction.release.value in releases]
-        case _ if args:
-            raise SanicException(
-                message=f"Request contained invalid paramater(s): {', '.join(f'{k!r}' for k in args.keys())}",
-                status_code=404,
-            )
-        case _:
-            results = [faction for faction in factions]
-
-    response = [result.to_dict() for result in results]
-    return sanic.json(response)
+@app.get("/")
+async def index() -> dict[str, str]:
+    return {
+        "info": "This is the index page of the ti4-mapgen API."
+        "You probably want to go to 'http://<hostname:port>/docs'.",
+    }
 
 
-@app.get("/tiles")
-async def tiles(request: sanic.Request) -> sanic.HTTPResponse:
-    tiles = cast(list[schema.Tile], schema.Tile.from_json_file("ti4_mapgen/data/tile_data.json"))
-    args = dict(request.args)
-
-    match args:
-        case _ if args:
-            raise SanicException(
-                message=f"Request contained invalid paramater(s): {', '.join(f'{k!r}' for k in args.keys())}",
-                status_code=404,
-            )
-        case _:
-            results = [tile for tile in tiles]
-
-    exclude = ["tag", "position"]
-    response = [result.to_dict(exclude=exclude) for result in results]
-    return sanic.json(response)
+@app.get("/maps/", response_model=list[Map])
+async def read_maps() -> list[MapInDB]:
+    async with AsyncBase(engine, "map") as base:
+        results = await base.fetch()
+    db_maps = [MapInDB(**result) for result in results.items]
+    return db_maps
 
 
-@app.get("/generate")
-async def generate(request: sanic.Request) -> sanic.HTTPResponse:
-    maps = cast(list[schema.Map], schema.Map.from_json_file("ti4_mapgen/data/map_data.json"))
-    tiles = cast(list[schema.Tile], schema.Tile.from_json_file("ti4_mapgen/data/tile_data.json"))
-    args = dict(request.args)
-
-    match args:
-        case {"release": releases, "players": players, "style": styles, "factions": names}:
-            layout = next((map.layout for map in maps if str(map.players.value) in players and map.style in styles))
-            stack = [tile for tile in tiles if tile.front.release.value in releases and tile.tag.value == "system"]
-            home_systems_tiles = [
-                tile
-                for tile in tiles
-                if tile.front.faction and tile.front.faction.value in names and tile.tag.value == "home"
-            ]
-        case {"release": releases, "players": players, "style": styles}:
-            layout = next((map.layout for map in maps if str(map.players.value) in players and map.style in styles))
-            stack = [tile for tile in tiles if tile.front.release.value in releases and tile.tag.value == "system"]
-            home_systems_tiles = []
-        case _:
-            param = ["release", "players", "style"]
-            raise SanicException(
-                message=f"Request missing paramater(s): {', '.join(f'{k!r}' for k in param if k not in args.keys())}",
-                status_code=404,
-            )
-
-    response = board.Board(layout=layout, stack=stack, home_system_tiles=home_systems_tiles)
-    exclude = ["stack"]
-    return sanic.json(response.to_dict(exclude=exclude))
+@app.get("/factions/", response_model=list[Faction])
+async def read_factions():
+    ...
 
 
-if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, workers=2, debug=True)
+@app.get("/tiles/", response_model=list[TileRead])
+async def read_tiles(query: TileQuery):
+    async with AsyncBase(engine, "tile") as base:
+        results = await base.fetch(**query.dict())
+    db_tiles = [TileInDB(**result) for result in results.items]
+    return db_tiles
+
+
+@app.get("/generate/")
+async def generate(
+    releases: list[str] = Form(),
+    players: int = Form(),
+    style: str = Form(),
+    factions: list[str] = Form(),
+):
+    ...
