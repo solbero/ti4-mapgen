73153e9b7c7f2dc4079109e6f387175e33b1b8d1
solbero <njord.solberg@gmail.com>
2022-07-24 18:50:08 +0200

    Update main.py to use app factory

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from ti4_mapgen.views import app
+import uvicorn
+
+from ti4_mapgen.app import create_app
+
+app = create_app()
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8005, log_level="info", reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level="info", reload=True)
